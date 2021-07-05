da9dab64126a93cbd4d99ff231748810070e6f46
Alex <collo@DESKTOP-0MTRO7E>
2021-07-05 11:06:02 +0200

    close modal with button

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -11,6 +11,8 @@ function editNav() {
 const modalbg = document.querySelector(".bground");
 const modalBtn = document.querySelectorAll(".modal-btn");
 const formData = document.querySelectorAll(".formData");
+const closeBtn = document.querySelectorAll(".close")
+closeBtn.forEach((btn) => btn.addEventListener('click', () => closeModal()))
 
 // launch modal event
 modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
@@ -18,6 +20,11 @@ modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
 // launch modal form
 function launchModal() {
   modalbg.style.display = "block";
+}
+
+//close modal form
+function closeModal() {
+  modalbg.style.display = "none";
 }
 
 
