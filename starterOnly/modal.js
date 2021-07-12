9ee1319e7579a8ffa66b731fa128cc07663ebc81
Alex <collo@DESKTOP-0MTRO7E>
2021-07-12 13:13:12 +0200

    rework logic of validators

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -20,7 +20,7 @@ modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
 
 // launch modal form
 function launchModal() {
-  modalbg.style.display = "block";
+  modalbg.style.display = "flex";
 }
 
 //close modal form
@@ -30,15 +30,15 @@ function closeModal() {
 
 function validate() {
   //we reset old errors
-  const errors = document.getElementsByClassName('error')
-  if (errors.length > 0) {
-    for (let i = 0; i < errors.length; i++) {
-      while (errors[i].firstChild) errors[i].removeChild(errors[i].firstChild);
-    }
-  }
+  removeErrors()
 
   const invalid = checkFormIsInvalid()
   console.log(invalid)
+  if (!invalid) {
+
+  } else {
+
+  }
 }
 
 /**
@@ -46,78 +46,40 @@ function validate() {
  * @returns boolean : errors
  */
 function checkFormIsInvalid() {
-  //DOM elements
-  const firstname = document.getElementById('first').value
-  const lastname = document.getElementById('last').value
-  const email = document.getElementById('email').value
-  const regexEmail = new RegExp("^[a-z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]{2,4}$")
-  const quantity = document.getElementById('quantity').value
-  const cgu = document.getElementById("checkbox1").checked
-  let error = false; 
+  let error = false;
+  const inputs = document.getElementsByClassName('checkedControl')
+  fetch('assets/formValidators.json').then((rep) => rep.json()).then((validators) => {
+    for (let i = 0; i < inputs.length; i++) {
+      let attr = inputs[i].name
+      //we need to check if cgu is checked so we test a wrong regex which always return false to trigger the checked verification
+      if (inputs[i].value.match(validators[attr].regex) || inputs[i].checked) {
+        console.log(attr)
+      } else {
+        createDomError(attr, validators[attr].error)
+        error = true
+      }
+    }
+    //we check radio now
+    if (!checkRadioIsValid()) {
+      let radioDOM = document.getElementsByName('location')
+      let span = document.createElement("span")
+      span.setAttribute('class', 'error')
+      span.innerHTML = 'Veuillez choisir une option.'
+      radioDOM[0].parentNode.appendChild(span)
+      error = true
+    }
+  })
+}
 
-  if (firstname.length < 2) {
-    let firstnameDOM = document.getElementById('first')
-    let span = document.createElement("span")
-    span.setAttribute('class', 'error')
-    span.innerHTML = "Veuillez entrer 2 caractères ou plus pour le champ du prénom."
-    firstnameDOM.parentNode.appendChild(span)
-    error = true
-  }
-
-  if (lastname.length < 2) {
-    let lastnameDOM = document.getElementById('last')
-    let span = document.createElement("span")
-    span.setAttribute('class', 'error')
-    span.innerHTML = "Veuillez entrer 2 caractères ou plus pour le champ du nom."
-    lastnameDOM.parentNode.appendChild(span)
-    error = true
-  }
-
-  if (!email.match(regexEmail)) {
-    let emailDOM = document.getElementById('email')
-    let span = document.createElement("span")
-    span.setAttribute('class', 'error')
-    span.innerHTML = "Veuillez entrer une adresse mail valide."
-    emailDOM.parentNode.appendChild(span)
-    error = true
-  }
-
-  if (!quantity) {
-    let quantityDOM = document.getElementById('quantity')
-    let span = document.createElement("span")
-    span.setAttribute('class', 'error')
-    span.innerHTML = "Veuillez renseigner votre nombre de tournois."
-    quantityDOM.parentNode.appendChild(span)
-    error = true
-  }
-
-  if (quantity > 99) {
-    let quantityDOM = document.getElementById('quantity')
-    let span = document.createElement("span")
-    span.setAttribute('class', 'error')
-    span.innerHTML = "Le nombre de tournois ne doit pas dépasser 99."
-    quantityDOM.parentNode.appendChild(span)
-    error = true
-  }
-
-  if (!checkRadioIsValid()) {
-    let radioDOM = document.getElementsByName('location')
-    let span = document.createElement("span")
-    span.setAttribute('class', 'error')
-    span.innerHTML = 'Veuillez choisir une option.'
-    radioDOM[0].parentNode.appendChild(span)
-    error = true
-  }
-
-  if (!cgu) {
-    let quantityDOM = document.getElementById('checkbox1')
-    let span = document.createElement("span")
-    span.setAttribute('class', 'error')
-    span.innerHTML = "Veuillez accepter les CGU."
-    quantityDOM.parentNode.appendChild(span)
-    error = true
-  }
-  return error
+function createDomError(name, error) {
+  console.log(name)
+  let dom = null;
+  if (name === 'cgu') dom = document.getElementById('checkbox1')
+  else dom = document.getElementById(name)
+  const span = document.createElement("span")
+  span.setAttribute('class', 'error')
+  span.innerHTML = error
+  dom.parentNode.appendChild(span)
 }
 
 /**
@@ -131,4 +93,13 @@ function checkRadioIsValid() {
     if (r.checked) countChecked++;
   })
   return countChecked > 0
+}
+
+function removeErrors() {
+  const errors = document.getElementsByClassName('error')
+  if (errors.length > 0) {
+    for (let i = 0; i < errors.length; i++) {
+      while (errors[i].firstChild) errors[i].removeChild(errors[i].firstChild);
+    }
+  }
 }
